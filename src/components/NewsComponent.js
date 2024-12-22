2afdbc545a4a2065023188a80a674fec965ab4b2
Vidwan Reddy <vidwanreddy0528@gmail.com>
2024-12-22 23:40:04 +0530

    creating cards as item components

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -4,10 +4,19 @@ import NewsItemComponent from './NewsItemComponent'
 export default class NewsComponent extends Component {
   render() {
     return (
-      <div>
-        This is a News Component
-        <NewsItemComponent/>
-        <NewsItemComponent/>
+      <div className='container my-3'>
+        <h2 className='my-4'>Thisisnews - Top headlines</h2>
+          <div className="row">
+              <div className='col-md-4'>
+              <NewsItemComponent title="myTitle" description="myDescription"/>
+              </div>
+              <div className='col-md-4'>
+              <NewsItemComponent title="myTitle" description="myDescription"/>
+              </div>
+              <div className='col-md-4'>
+              <NewsItemComponent title="myTitle" description="myDescription"/>
+              </div>
+          </div>
         
       </div>
     )
