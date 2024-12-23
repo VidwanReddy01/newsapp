5ba651ec16335c811f9561a1fb785037b9917f82
Vidwan Reddy <vidwanreddy0528@gmail.com>
2024-12-23 13:21:41 +0530

    get news from news API and show

--- src/components/NewsItemComponent.js
+++ src/components/NewsItemComponent.js
@@ -6,7 +6,7 @@ export default class NewsItemComponent extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{maxHeight:'28rem', minHeight:'28rem'}}>
-        <img src={imageUrl} className="card-img-top" style={{maxHeight:'15rem', minHeight:'15rem'}} alt="..." />
+        <img src={!imageUrl?"https://img.freepik.com/free-vector/breaking-news-information-concept_52683-36243.jpg?t=st=1734939706~exp=1734943306~hmac=f6ae2a2257119fb9ce69d4690ab43eeb415dae85bdbe1c81b22ccdc22acc86e0&w=2000": imageUrl} className="card-img-top" style={{maxHeight:'15rem', minHeight:'15rem'}} alt="..." />
         <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
